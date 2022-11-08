9754b0cf13bfd9a276280d8c8a872aef1d609735
Eunji Roh <nonjee888@gmail.com>
2022-11-09 00:02:50 +0900

    [feat]MenuBar done

--- src/shared/MenuBar.tsx
+++ src/shared/MenuBar.tsx
@@ -1,5 +1,49 @@
+import "./style.scss";
+import user from "../asset/images/user.png";
+import chat from "../asset/images/chat.png";
+import more from "../asset/images/more.png";
+import selectedUser from "../asset/images/selectedUser.png";
+import selectedChat from "../asset/images/selectedChat.png";
+import selectedMore from "../asset/images/selectedMore.png";
+import { useNavigate } from "react-router-dom";
+
 const MenuBar = () => {
-  return <div>MenuBar</div>;
+  const navigate = useNavigate();
+  const pathName = window.location.pathname;
+  return (
+    <div className="menu">
+      <div>
+        <img
+          src={pathName === "/friends" ? selectedUser : user}
+          className="userIcon"
+          alt="userIcon"
+          onClick={() => {
+            navigate("/friends");
+          }}
+        />
+      </div>
+      <div>
+        <img
+          src={pathName === "/chatlist" ? selectedChat : chat}
+          className="userIcon"
+          alt="userIcon"
+          onClick={() => {
+            navigate("/chatlist");
+          }}
+        />
+      </div>
+      <div>
+        <img
+          src={pathName === "/myinfo" ? selectedMore : more}
+          className="moreIcon"
+          alt="userIcon"
+          onClick={() => {
+            navigate("/myinfo");
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 
 export default MenuBar;
